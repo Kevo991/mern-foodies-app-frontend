2190855f3dcf12d7b2c21e32012a724406167bf4
dixon <dixonkelvin98@gmail.com.com>
2024-07-27 16:30:04 +0100

    first commit

---
+++ src/Hero.tsx
@@ -0,0 +1,12 @@
+import hero from "../src/assets/hero.jpg";
+
+const Hero = () => {
+    return (
+    
+            <img  src={hero} className="w-full max-h-[600px] object-cover" />
+         
+    )
+}
+
+ 
+export default Hero;
